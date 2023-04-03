55f3891259221307318c10b0472aa945966a8fbc
Jim Claverhouse <116584138+JC306010@users.noreply.github.com>
2023-04-03 18:57:17 -0500

    Added the python file

---
+++ print('Welcome to Jons Mad Libs Program!.py
@@ -0,0 +1,8 @@
+print('Welcome to Jons Mad Libs Program!')
+first_person = (input('write a name for the first person: '))
+second_person = (input('write a name for the second person: '))
+activity = (input('write an activity: '))
+place = (input('write a place: '))
+item = (input('write an item: '))
+excuse = (input('write an excuse: '))
+print('Once upon a time', (first_person), 'went to the', (place), 'to', (activity), 'but on his way', (second_person), 'someone', (first_person), 'had not seen in a long time wanted to say Hi and catch up with', (first_person), 'but', (first_person), 'was not very fond of', (second_person), 'so', (first_person), 'tried to leave the conversation by saying', (excuse), 'and', (second_person), 'bought it and let', (first_person), 'leave and be on his way.', 'when', (first_person), 'got to', (place), 'He realized He forgot', (item), (first_person), 'says Oh well I did not really want to do', (activity), 'anyways!')
